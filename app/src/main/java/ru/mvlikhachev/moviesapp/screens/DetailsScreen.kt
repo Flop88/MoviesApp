84cb6d05abfec29a4a69735bffc579dd13160ea6
MV.Likhachev <m.lihachev@paysend.com>
2022-04-26 21:05:35 +0200

    Lesson-4

---
+++ app/src/main/java/ru/mvlikhachev/moviesapp/screens/DetailsScreen.kt
@@ -0,0 +1,11 @@
+package ru.mvlikhachev.moviesapp.screens
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import ru.mvlikhachev.moviesapp.MainViewModel
+
+@Composable
+fun DetailsScreen(navController: NavController, viewModel: MainViewModel, itemId: String) {
+    Text("Details screen: item id: ${itemId}")
+}
